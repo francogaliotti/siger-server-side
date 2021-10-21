460f6aa92c260c3f9c3c949081178337315b7864
francogaliotti <67112060+francogaliotti@users.noreply.github.com>
2021-10-21 10:29:41 -0300

    Nueva Implementacion de controller, repository y service en ZonaInhospita, + actualizacion de EstadoLicenciaController

--- src/main/java/com/SIGER/SIGER/servicesImpl/ZonaInhospitaServiceImpl.java
+++ src/main/java/com/SIGER/SIGER/servicesImpl/ZonaInhospitaServiceImpl.java
@@ -33,8 +33,8 @@ public class ZonaInhospitaServiceImpl extends BaseServiceImpl<ZonaInhospita, Lon
 		return zonaInhospitaRepository.existsById(id);
 	}
 
-	public boolean existsByDenominacionZona (String denominacion) {
-		return zonaInhospitaRepository.existsByDenominacionZona(denominacion);
+	public boolean existsByDenominacionZona (String den) {
+		return zonaInhospitaRepository.existsByDenominacionZona(den);
 	}
 
 }
