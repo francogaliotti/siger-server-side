0054c2a2caffd334ce50c9718f5577774101bae0
alexB0999 <alexbahi.99@gmail.com>
2021-12-24 16:28:40 -0300

    Error fixing: @Component label added on RemuneracionExpert

--- src/main/java/com/SIGER/SIGER/BI/RemuneracionExpert.java
+++ src/main/java/com/SIGER/SIGER/BI/RemuneracionExpert.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Component
 public class RemuneracionExpert extends
     AbsBaseExpert<Remuneracion, RemuneracionService, RemuneracionRequest, RemuneracionResponse> {
 
