fec2d510e0d78abf7dff2b0c37fde6d62368d041
alexB0999 <alexbahi.99@gmail.com>
2021-12-23 23:44:13 -0300

    Error fixing for UsersSeeder

--- src/main/java/com/SIGER/SIGER/security/seeder/UsersSeeder.java
+++ src/main/java/com/SIGER/SIGER/security/seeder/UsersSeeder.java
@@ -1,10 +1,10 @@
 package com.SIGER.SIGER.security.seeder;
 
-import com.SIGER.SIGER.security.seeder.config.ApplicationRole;
 import com.SIGER.SIGER.security.entity.Rol;
 import com.SIGER.SIGER.security.entity.Usuario;
 import com.SIGER.SIGER.security.repository.RolRepository;
 import com.SIGER.SIGER.security.repository.UsuarioRepository;
+import com.SIGER.SIGER.security.seeder.config.ApplicationRole;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,13 +17,13 @@ public class UsersSeeder implements CommandLineRunner {
   private static final String PHOTO = "https://foo.jpg";
   private static final long ROLE_USER = 1L;
   private static final long ROLE_ADMIN = 2L;
-  private static final String PASSWORD_GENERIC = "siger1234";
+  private static final String PASSWORD_GENERIC = "foo12345";
 
   @Autowired
-  UsuarioRepository userRepository;
+  UsuarioRepository usuarioRepository;
 
   @Autowired
-  RolRepository roleRepository;
+  RolRepository rolRepository;
 
   @Autowired
   BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -35,17 +35,17 @@ public class UsersSeeder implements CommandLineRunner {
   }
 
   private void loadRoles() {
-    if (roleRepository.count() == 0) {
-      roleRepository.save(buildRole(ApplicationRole.USER));
-      roleRepository.save(buildRole(ApplicationRole.ADMIN));
+    if (rolRepository.count() == 0) {
+      rolRepository.save(buildRole(ApplicationRole.USER));
+      rolRepository.save(buildRole(ApplicationRole.ADMIN));
     }
   }
 
   private Rol buildRole(ApplicationRole applicationRole) {
-    Rol role = new Rol();
-    role.setId(getRoleId(applicationRole));
-    role.setRolNombre(applicationRole.getFullRoleName());
-    return role;
+    Rol rol = new Rol();
+    rol.setId(getRoleId(applicationRole));
+    rol.setName(applicationRole.getFullRoleName());
+    return rol;
   }
 
   private long getRoleId(ApplicationRole applicationRole) {
@@ -53,24 +53,27 @@ public class UsersSeeder implements CommandLineRunner {
   }
 
   private void loadUsers() {
-    if (userRepository.count() == 0) {
+    if (usuarioRepository.count() == 0) {
       loadUsersWithRoleUser();
       loadUsersWithRoleAdmin();
     }
   }
 
   private void loadUsersWithRoleUser() {
-    userRepository.save(buildUser("Jeremias Fernandez", "JFernandez", "jfernandez@siger.com"));
-    userRepository.save(buildUser( "Alexis Bahi", "ABahi", "abahi@siger.com"));
-    userRepository.save(buildUser( "Franco Galiotti", "FGaliotti", "fgaliotti@siger.com"));
-    userRepository.save(buildUser( "Diego Villa", "DVilla", "dvilla@siger.com"));
+    usuarioRepository.save(buildUser("Jeremias Fernandez", "JFernandez", "jfernandez@siger.com"));
+    usuarioRepository.save(buildUser("Alexis Bahi", "ABahi", "abahi@siger.com"));
+    usuarioRepository.save(buildUser("Franco Galiotti", "FGaliotti", "fgaliotti@siger.com"));
+    usuarioRepository.save(buildUser("Diego Villa", "DVilla", "dvilla@siger.com"));
   }
 
   private void loadUsersWithRoleAdmin() {
-    userRepository.save(buildUserAdmin("Jeremias Fernandez", "Jeremias.Fernandez", "fernandez.jeremias.daniel@gmail.com"));
-    userRepository.save(buildUserAdmin( "Alexis Bahi", "Alexis.Bahi", "abahi.99@gmail.com"));
-    userRepository.save(buildUserAdmin( "Franco Galiotti", "Franco.Galiotti", "francogaliotti@gmail.com"));
-    userRepository.save(buildUserAdmin( "Diego Villa", "Diego.Villa", "diegovillautnfrm@gmail.com"));
+    usuarioRepository.save(buildUserAdmin("Jeremias Fernandez", "Jeremias.Fernandez",
+        "fernandez.jeremias.daniel@gmail.com"));
+    usuarioRepository.save(buildUserAdmin("Alexis Bahi", "Alexis.Bahi", "abahi.99@gmail.com"));
+    usuarioRepository.save(
+        buildUserAdmin("Franco Galiotti", "Franco.Galiotti", "francogaliotti@gmail.com"));
+    usuarioRepository.save(
+        buildUserAdmin("Diego Villa", "Diego.Villa", "diegovillautnfrm@gmail.com"));
   }
 
   private Usuario buildUser(String nombre, String username, String email) {
@@ -79,7 +82,7 @@ public class UsersSeeder implements CommandLineRunner {
         .username(username)
         .email(email)
         .password(bCryptPasswordEncoder.encode(PASSWORD_GENERIC))
-        .roles(Set.of(roleRepository.findById(ROLE_USER).get()))
+        .roles(Set.of(rolRepository.findById(ROLE_USER).get()))
         .build();
   }
 
@@ -89,7 +92,7 @@ public class UsersSeeder implements CommandLineRunner {
         .username(username)
         .email(email)
         .password(bCryptPasswordEncoder.encode(PASSWORD_GENERIC))
-        .roles(Set.of(roleRepository.findById(ROLE_USER).get()))
+        .roles(Set.of(rolRepository.findById(ROLE_ADMIN).get()))
         .build();
   }
 
