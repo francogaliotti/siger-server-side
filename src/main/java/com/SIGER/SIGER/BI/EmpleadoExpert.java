29ff65c8cb764f1b8f14e30bce163243add14a91
alexB0999 <alexbahi.99@gmail.com>
2022-01-11 18:27:38 -0300

    User register

--- src/main/java/com/SIGER/SIGER/BI/EmpleadoExpert.java
+++ src/main/java/com/SIGER/SIGER/BI/EmpleadoExpert.java
@@ -2,6 +2,8 @@ package com.SIGER.SIGER.BI;
 
 import com.SIGER.SIGER.common.Message;
 import com.SIGER.SIGER.common.PaginatedResultsHeaderUtils;
+import com.SIGER.SIGER.emailSender.controller.EmailController;
+import com.SIGER.SIGER.emailSender.dto.EmailValuesDTO;
 import com.SIGER.SIGER.model.entities.DocumentoIdentidad;
 import com.SIGER.SIGER.model.entities.Empleado;
 import com.SIGER.SIGER.model.entities.Nacionalidad;
@@ -9,17 +11,23 @@ import com.SIGER.SIGER.model.entities.TipoDocumento;
 import com.SIGER.SIGER.model.requests.EmpleadoRequest;
 import com.SIGER.SIGER.model.responses.EmpleadoResponse;
 import com.SIGER.SIGER.security.entity.Usuario;
+import com.SIGER.SIGER.security.expert.AuthExpert;
 import com.SIGER.SIGER.security.service.UsuarioService;
 import com.SIGER.SIGER.services.EmpleadoService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.Random;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -35,6 +43,28 @@ public class EmpleadoExpert extends
 
   @Autowired
   UsuarioService userService;
+
+  @Autowired
+  AuthExpert authExpert;
+
+  @Autowired
+  EmailController emailController;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Value("${lowerCasesAmount}")
+  int lowerCasesAmount;
+
+  @Value("${capitalLettersAmount}")
+  int capitalLettersAmount;
+
+  @Value("${digitsAmount}")
+  int digitsAmount;
+
+  @Value("${nonAlphanumericsAmount}")
+  int nonAlphanumericsAmount;
+
 
   @Override
   public ResponseEntity<List<EmpleadoResponse>> findAll(int page, int size,
@@ -82,7 +112,8 @@ public class EmpleadoExpert extends
 
     Nacionalidad nationality = new Nacionalidad();
     nationality.setId(empleadoRequest.getNacionalidad().getId());
-    DocumentoIdentidad identityCard = DocumentoIdentidad.builder().nroIdentidad(empleadoRequest.getNroIdentificacionPersonal()).build();
+    DocumentoIdentidad identityCard = DocumentoIdentidad.builder()
+        .nroIdentidad(empleadoRequest.getNroIdentificacionPersonal()).build();
     TipoDocumento typeOfDocument = new TipoDocumento();
     typeOfDocument.setId(empleadoRequest.getDocumentoIdentidad().getTipoDocumento().getId());
     identityCard.setTipoDocumento(typeOfDocument);
@@ -148,11 +179,124 @@ public class EmpleadoExpert extends
 
     Optional<Usuario> optionalUser = userService.getByUsername(username);
     Usuario user = null;
-    if(optionalUser.isPresent()) user = optionalUser.get();
+    if (optionalUser.isPresent()) {
+      user = optionalUser.get();
+    }
 
     Empleado empleado = empleadoService.getByfk_usuario(user.getId());
     EmpleadoResponse empleadoResponse = modelMapper.map(empleado, EmpleadoResponse.class);
     return new ResponseEntity(empleadoResponse, HttpStatus.OK);
   }
+
+  public void createUser(EmpleadoRequest empleadoRequest) throws Exception {
+
+    String aux_password = "";
+    ModelMapper mapper = new ModelMapper();
+    Empleado employee = mapper.map(empleadoRequest, Empleado.class);
+
+    aux_password = this.generateRandomPassword();
+
+    employee.getUsuario().setPassword(passwordEncoder.encode(aux_password));
+    empleadoService.save(employee);
+    emailController.sendEmail(
+        this.preparingEmailData(employee.getUsuario().getUsername(), aux_password,
+            employee.getCorreoPersonal()));
+
+  }
+
+  private EmailValuesDTO preparingEmailData(String username, String password,
+      String personalEmail) {
+    EmailValuesDTO emailValuesDTO = EmailValuesDTO.builder().username(username).password(password)
+        .mailTo(personalEmail).build();
+
+    return emailValuesDTO;
+  }
+
+  private String generateRandomPassword() {
+
+    String lowCases = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+    String capitalLetters = lowCases.toUpperCase(Locale.ROOT);
+    String digits = "0,1,2,3,4,5,6,7,8,9";
+    String nonAlphanumerics = "!,#,$,%,^,&,/,?,Â¿,-,<,>";
+
+    String[] lowCasesArray = lowCases.split(",");
+    String[] capitalLettersArray = capitalLetters.split(",");
+    String[] digitsArray = digits.split(",");
+    String[] nonAlphanumericsArray = nonAlphanumerics.split(",");
+
+    Random rand = new Random();
+    String password = "";
+    String newPassword = "";
+    String passwordArray[];
+
+    if (this.lowerCasesAmount > 0) {
+      for (int counter = 0; counter < this.lowerCasesAmount; counter++) {
+        if (counter == 0) {
+          password = lowCasesArray[rand.nextInt(lowCasesArray.length)];
+        } else {
+          password = lowCasesArray[rand.nextInt(lowCasesArray.length)] + "," + password;
+        }
+      }
+    }
+
+    if (this.capitalLettersAmount > 0) {
+      for (int counter = 0; counter < this.capitalLettersAmount; counter++) {
+        password = capitalLettersArray[rand.nextInt(capitalLettersArray.length)] + "," + password;
+      }
+    }
+
+    if (this.digitsAmount > 0) {
+      for (int counter = 0; counter < this.digitsAmount; counter++) {
+        password = digitsArray[rand.nextInt(digitsArray.length)] + "," + password;
+      }
+    }
+
+    if (this.nonAlphanumericsAmount > 0) {
+      for (int counter = 0; counter < this.nonAlphanumericsAmount; counter++) {
+        password =
+            nonAlphanumericsArray[rand.nextInt(nonAlphanumericsArray.length)] + "," + password;
+      }
+    }
+
+    passwordArray = password.split(",");
+
+    if (passwordArray.length > 0) {
+      int[] exist = new int[passwordArray.length];
+
+      for (int setter = 0; setter < exist.length; setter++) {
+        exist[setter] = exist.length + 10;
+      }
+
+      int counter = 0;
+
+      for (String letter : passwordArray) {
+        int winner = 0;
+        boolean passed = false;
+
+        do {
+          winner = rand.nextInt(passwordArray.length);
+
+          for (int num : exist) {
+            if (winner != num) {
+              passed = true;
+            } else {
+              passed = false;
+              break;
+            }
+          }
+
+          if (passed) {
+            newPassword += passwordArray[winner];
+
+            exist[counter] = winner;
+            counter += 1;
+          }
+
+        } while (!passed);
+      }
+    }
+    return newPassword;
+  }
+
 
 }
