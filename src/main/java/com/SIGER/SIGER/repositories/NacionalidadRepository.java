b021bf902a2d01151de00d3aaa5393f7c099b52d
alexB0999 <alexbahi.99@gmail.com>
2022-01-11 18:33:33 -0300

    Modification on Nacionalidad, new attributes added in order to match with csv file upload. Pagination method added. Seeder to read csv file and upload registers to database. nacionalidad.csv added to make seeder works

--- src/main/java/com/SIGER/SIGER/repositories/NacionalidadRepository.java
+++ src/main/java/com/SIGER/SIGER/repositories/NacionalidadRepository.java
@@ -1,9 +1,12 @@
 package com.SIGER.SIGER.repositories;
 
 import com.SIGER.SIGER.model.entities.Nacionalidad;
-import com.SIGER.SIGER.servicesInterfaces.BaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NacionalidadRepository extends BaseRepository<Nacionalidad, Long> {
+
+  Page<Nacionalidad> findAll(Pageable pageable);
 }
