c240446ba5c874441aa1717041f8ad81d7bb66b8
francogaliotti <67112060+francogaliotti@users.noreply.github.com>
2022-02-04 11:50:42 -0300

    Creacion de licencia y boleta, correccion de bugs IV

--- src/main/java/com/SIGER/SIGER/security/repository/UsuarioRepository.java
+++ src/main/java/com/SIGER/SIGER/security/repository/UsuarioRepository.java
@@ -1,23 +1,16 @@
 package com.SIGER.SIGER.security.repository;
 
 import com.SIGER.SIGER.security.entity.Usuario;
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByUsername(String username);
-
     Optional<Usuario> findByUsernameOrCorreoInstitucional(String username, String correoInstitucional);
-
     Optional<Usuario> findByTokenPassword(String tokenPassword);
-
     boolean existsByUsername(String username);
-
     boolean existsByCorreoInstitucional(String correoInstitucional);
-
 }
