1be79d1571138d64c5066743014adea5f30c6550
Alexis Bahi <alexbahi_99@yahoo.com.ar>
2022-02-05 17:47:00 -0300

    Adding change-password-email-template.html modifying email-template.html and successfull-register-template.html templates Renaming PasswordDTO Adding sendChangePasswordEmail method at EmailController and EmailService

--- src/main/java/com/SIGER/SIGER/emailSender/controller/EmailController.java
+++ src/main/java/com/SIGER/SIGER/emailSender/controller/EmailController.java
@@ -1,7 +1,7 @@
 package com.SIGER.SIGER.emailSender.controller;
 
 import com.SIGER.SIGER.common.Message;
-import com.SIGER.SIGER.emailSender.dto.ChangePasswordDTO;
+import com.SIGER.SIGER.emailSender.dto.ChangeAndResetPasswordDTO;
 import com.SIGER.SIGER.emailSender.dto.EmailValuesDTO;
 import com.SIGER.SIGER.emailSender.service.EmailService;
 import com.SIGER.SIGER.security.entity.Usuario;
@@ -38,37 +38,14 @@ public class EmailController {
   @Value("${spring.mail.username}")
   String mailFrom;
 
-  @Value("${mail.subject-Change-Password}")
-  String subject_Change_Password;
-
   @Value("${mail.subject-welcome}")
   String subject_Welcome;
 
-  @PostMapping("/send-email")
-  public ResponseEntity<?> sendEmail(@RequestBody EmailValuesDTO valuesDTO){
-    Optional<Usuario> usuarioOptional = usuarioService.getByUsernameOrCorreoInstitucional(valuesDTO.getMailTo());
-    if(!usuarioOptional.isPresent()){
-      return new ResponseEntity<>(new Message("No existe ningún usuario con esas credenciales"),
-          HttpStatus.NOT_FOUND);
-    }
+  @Value("${mail.subject-Reset-Password}")
+  String subject_Reset_Password;
 
-    Usuario usuario = usuarioOptional.get();
-    valuesDTO.setMailFrom(mailFrom);
-    valuesDTO.setMailTo(usuario.getCorreoInstitucional());
-    valuesDTO.setSubject(subject_Change_Password);
-    valuesDTO.setUsername(usuario.getUsername());
-
-    UUID uuid = UUID.randomUUID();
-    String tokenPassword = uuid.toString();
-
-    valuesDTO.setTokenPassword(tokenPassword);
-    usuario.setTokenPassword(tokenPassword);
-
-    usuarioService.save(usuario);
-    emailService.sendEmail(valuesDTO);
-
-    return new ResponseEntity<>(new Message("Te hemos enviado un correo"), HttpStatus.OK);
-  }
+  @Value("${mail.subject-Change-Password}")
+  String subject_Change_Password;
 
   @PostMapping("/send-welcome-email")
   public ResponseEntity<?> sendWelcomeEmail(@RequestBody EmailValuesDTO valuesDTO){
@@ -96,22 +73,74 @@ public class EmailController {
     return new ResponseEntity<>(new Message("Te hemos enviado un correo"), HttpStatus.OK);
   }
 
+  @PostMapping("/send-email")
+  public ResponseEntity<?> sendResetPasswordEmail(@RequestBody EmailValuesDTO valuesDTO){
+    Optional<Usuario> usuarioOptional = usuarioService.getByUsernameOrCorreoInstitucional(valuesDTO.getMailTo());
+    if(!usuarioOptional.isPresent()){
+      return new ResponseEntity<>(new Message("No existe ningún usuario con esas credenciales"),
+          HttpStatus.NOT_FOUND);
+    }
+
+    Usuario usuario = usuarioOptional.get();
+    valuesDTO.setMailFrom(mailFrom);
+    valuesDTO.setMailTo(usuario.getCorreoInstitucional());
+    valuesDTO.setSubject(subject_Reset_Password);
+    valuesDTO.setUsername(usuario.getUsername());
+
+    UUID uuid = UUID.randomUUID();
+    String tokenPassword = uuid.toString();
+
+    valuesDTO.setTokenPassword(tokenPassword);
+    usuario.setTokenPassword(tokenPassword);
+
+    usuarioService.save(usuario);
+    emailService.sendResetPasswordEmail(valuesDTO);
+
+    return new ResponseEntity<>(new Message("Te hemos enviado un correo"), HttpStatus.OK);
+  }
+
+  @PostMapping("/send-change-password-email")
+  public ResponseEntity<?> sendChangePasswordEmail(@RequestBody EmailValuesDTO valuesDTO){
+    Optional<Usuario> usuarioOptional = usuarioService.getByUsernameOrCorreoInstitucional(valuesDTO.getMailTo());
+    if(!usuarioOptional.isPresent()){
+      return new ResponseEntity<>(new Message("No existe ningún usuario con esas credenciales"),
+              HttpStatus.NOT_FOUND);
+    }
+
+    Usuario usuario = usuarioOptional.get();
+    valuesDTO.setMailFrom(mailFrom);
+    valuesDTO.setMailTo(usuario.getCorreoInstitucional());
+    valuesDTO.setSubject(subject_Change_Password);
+    valuesDTO.setUsername(usuario.getUsername());
+
+    UUID uuid = UUID.randomUUID();
+    String tokenPassword = uuid.toString();
+
+    valuesDTO.setTokenPassword(tokenPassword);
+    usuario.setTokenPassword(tokenPassword);
+
+    usuarioService.save(usuario);
+    emailService.sendChangePasswordEmail(valuesDTO);
+
+    return new ResponseEntity<>(new Message("Te hemos enviado un correo"), HttpStatus.OK);
+  }
+
   @PostMapping("/change-password")
-  public ResponseEntity<?> changePassword(@Valid@RequestBody ChangePasswordDTO changePasswordDTO,
-      BindingResult bindingResult){
+  public ResponseEntity<?> resetPassword(@Valid@RequestBody ChangeAndResetPasswordDTO changeAndResetPasswordDTO,
+                                         BindingResult bindingResult){
     if(bindingResult.hasErrors()){
       return new ResponseEntity<>(new Message("Campos mal puestos"), HttpStatus.BAD_REQUEST);
     }
-    if(!changePasswordDTO.getPassword().equals(changePasswordDTO.getConfirmPassword())){
+    if(!changeAndResetPasswordDTO.getPassword().equals(changeAndResetPasswordDTO.getConfirmPassword())){
       return new ResponseEntity<>(new Message("Las contraseñas no coinciden"), HttpStatus.BAD_REQUEST);
     }
-    Optional<Usuario>usuarioOptional = usuarioService.findByTokenPassword(changePasswordDTO.getTokenPassword());
+    Optional<Usuario>usuarioOptional = usuarioService.findByTokenPassword(changeAndResetPasswordDTO.getTokenPassword());
     if(!usuarioOptional.isPresent()){
       return new ResponseEntity<>(new Message("No existe ningún usuario con esas credenciales"),
           HttpStatus.NOT_FOUND);
     }
     Usuario usuario = usuarioOptional.get();
-    String newPassword = passwordEncoder.encode(changePasswordDTO.getPassword());
+    String newPassword = passwordEncoder.encode(changeAndResetPasswordDTO.getPassword());
     usuario.setPassword(newPassword);
     usuario.setTokenPassword(null);
     usuarioService.save(usuario);
