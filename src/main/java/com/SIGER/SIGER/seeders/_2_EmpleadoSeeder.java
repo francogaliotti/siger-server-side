234da8d8d7e2e735b178cbae088d749308b54764
Alexis Bahi <alexbahi_99@yahoo.com.ar>
2022-02-07 14:45:39 -0300

    Adding domicilio and remanente dias licencia to _2_EmpleadoSeeder

--- src/main/java/com/SIGER/SIGER/seeders/_2_EmpleadoSeeder.java
+++ src/main/java/com/SIGER/SIGER/seeders/_2_EmpleadoSeeder.java
@@ -6,15 +6,15 @@ import com.SIGER.SIGER.datos_gob_ar.entities.Provincia;
 import com.SIGER.SIGER.datos_gob_ar.repositories.DepartamentoRepository;
 import com.SIGER.SIGER.datos_gob_ar.repositories.LocalidadRepository;
 import com.SIGER.SIGER.datos_gob_ar.repositories.ProvinciaRepository;
-import com.SIGER.SIGER.model.entities.Domicilio;
-import com.SIGER.SIGER.model.entities.Empleado;
-import com.SIGER.SIGER.model.entities.Sector;
-import com.SIGER.SIGER.repositories.EmpleadoRepository;
-import com.SIGER.SIGER.repositories.SectorRepository;
+import com.SIGER.SIGER.model.entities.*;
+import com.SIGER.SIGER.repositories.*;
 import com.SIGER.SIGER.security.entity.Usuario;
 import com.SIGER.SIGER.security.repository.UsuarioRepository;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +34,18 @@ public class _2_EmpleadoSeeder implements CommandLineRunner {
     SectorRepository sectorRepository;
 
     @Autowired
+    TipoLicenciaRepository tipoLicenciaRepository;
+
+    @Autowired
+    RemanenteDiasLicenciasRepository remanenteDiasLicenciasRepository;
+
+    @Autowired
+    RemuneracionRepository remuneracionRepository;
+
+    @Autowired
+    DomicilioRepository domicilioRepository;
+
+    @Autowired
     ProvinciaRepository provinciaRepository;
 
     @Autowired
@@ -42,15 +54,20 @@ public class _2_EmpleadoSeeder implements CommandLineRunner {
     @Autowired
     LocalidadRepository localidadRepository;
 
-    private Domicilio domicilio;
-
 
     @Override
     public void run(String... args) throws Exception {
-        loadEmpleados();
+        if (empleadoRepository.count() == 0) {
+            loadDomicilio();
+            loadEmpleados();
+        }
     }
 
     private void loadDomicilio() {
+        domicilioRepository.save(buildDomicilio());
+    }
+
+    private Domicilio buildDomicilio() {
 
         Optional<Provincia> optionalProvincia = provinciaRepository.findById("50");
 
@@ -58,7 +75,7 @@ public class _2_EmpleadoSeeder implements CommandLineRunner {
 
         Optional<Localidad> optionalLocalidad = localidadRepository.findById("50007010001");
 
-        this.domicilio = Domicilio.builder()
+        return Domicilio.builder()
                 .calle("Calle Falsa")
                 .nroCalle(123)
                 .provincia(optionalProvincia.get())
@@ -68,29 +85,28 @@ public class _2_EmpleadoSeeder implements CommandLineRunner {
     }
 
     private void loadEmpleados() {
-        if (empleadoRepository.count() == 0) {
-            this.loadDomicilio();
-            empleadoRepository.save(
-                    buildEmpleado("Jeremias", "Fernandez", "jfernandez@siger.com", 1, "1", false, Long.valueOf(1),Long.valueOf(2)));
-            empleadoRepository.save(buildEmpleado("Alexis", "Bahi", "abahi@siger.com", 1, "2", false, Long.valueOf(2),Long.valueOf(3)));
-            empleadoRepository.save(
-                    buildEmpleado("Franco", "Galiotti", "fgaliotti@siger.com", 1, "3", false, Long.valueOf(3),Long.valueOf(4)));
-            empleadoRepository.save(buildEmpleado("Diego", "Villa", "dvilla@siger.com", 1, "4", false, Long.valueOf(4),Long.valueOf(5)));
-            empleadoRepository.save(
-                    buildEmpleado("Jeremias", "Fernandez", "fernandez.jeremias.daniel@gmail.com", 1, "5", true,
-                            Long.valueOf(5),Long.valueOf(1)));
-            empleadoRepository.save(buildEmpleado("Alexis", "Bahi", "abahi.99@gmail.com", 1, "6", true, Long.valueOf(6),Long.valueOf(1)));
-            empleadoRepository.save(
-                    buildEmpleado("Franco", "Galiotti", "francogaliotti@gmail.com", 1, "7", true, Long.valueOf(7),Long.valueOf(1)));
-            empleadoRepository.save(
-                    buildEmpleado("Diego", "Villa", "diegovillautnfrm@gmail.com", 1, "8", true, Long.valueOf(8),Long.valueOf(1)));
-        }
+        empleadoRepository.save(
+                buildEmpleado("Jeremias", "Fernandez", "jfernandez@siger.com", 1, "1", false, Long.valueOf(1), Long.valueOf(2), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(buildEmpleado("Alexis", "Bahi", "abahi@siger.com", 1, "2", false, Long.valueOf(2), Long.valueOf(3), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(
+                buildEmpleado("Franco", "Galiotti", "fgaliotti@siger.com", 1, "3", false, Long.valueOf(3), Long.valueOf(4), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(buildEmpleado("Diego", "Villa", "dvilla@siger.com", 1, "4", false, Long.valueOf(4), Long.valueOf(5), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(
+                buildEmpleado("Jeremias", "Fernandez", "fernandez.jeremias.daniel@gmail.com", 1, "5", true,
+                        Long.valueOf(5), Long.valueOf(1), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(buildEmpleado("Alexis", "Bahi", "abahi.99@gmail.com", 1, "6", true, Long.valueOf(6), Long.valueOf(1), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(
+                buildEmpleado("Franco", "Galiotti", "francogaliotti@gmail.com", 1, "7", true, Long.valueOf(7), Long.valueOf(1), Long.valueOf(1), Long.valueOf(2)));
+        empleadoRepository.save(
+                buildEmpleado("Diego", "Villa", "diegovillautnfrm@gmail.com", 1, "8", true, Long.valueOf(8), Long.valueOf(1), Long.valueOf(1), Long.valueOf(2)));
     }
 
     private Empleado buildEmpleado(String nombre, String apellido, String correoPersonal,
-                                   int estadoCivil, String legajo, boolean esEncargado, Long usuarioId, Long sectorId) {
+                                   int estadoCivil, String legajo, boolean esEncargado, Long usuarioId, Long sectorId, Long domicilioId, Long remuneracionId) {
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(usuarioId);
         Optional<Sector> optionalSector = sectorRepository.findById(sectorId);
+        Optional<Remuneracion> optionalRemuneracion = remuneracionRepository.findById(remuneracionId);
+        Optional<Domicilio> optionalDomicilio = domicilioRepository.findById(domicilioId);
         Empleado empleado = Empleado.builder()
                 .fechaAlta(new Date())
                 .nombre(nombre)
@@ -103,16 +119,34 @@ public class _2_EmpleadoSeeder implements CommandLineRunner {
                 .esEncargado(esEncargado)
                 //.nroTelefonoFijo()
                 //.nroTelefonoCelular()
-                .domicilio(domicilio)
+                .remanenteDiasLicencias(buildAndSetRemanenteDiasLicencia())
                 .build();
 
-        if (optionalUsuario.isPresent()){
+        if (optionalUsuario.isPresent()) {
             empleado.setUsuario(optionalUsuario.get());
         }
-        if (optionalSector.isPresent()){
+        if (optionalSector.isPresent()) {
             empleado.setSector(optionalSector.get());
         }
+        if (optionalRemuneracion.isPresent()) {
+            empleado.setRemuneracion(optionalRemuneracion.get());
+        }
+        if (optionalDomicilio.isPresent()) {
+            empleado.setDomicilio(optionalDomicilio.get());
+        }
+
         return empleado;
+    }
+
+    public List<RemanenteDiasLicencia> buildAndSetRemanenteDiasLicencia() {
+        List<TipoLicencia> tipoLicencias = tipoLicenciaRepository.findAll();
+        List<RemanenteDiasLicencia> remanenteDiasLicencias = new ArrayList<>();
+        for (int i = 0; i < tipoLicencias.size(); i++) {
+            RemanenteDiasLicencia remanenteDiasLicencia = new RemanenteDiasLicencia(LocalDate.now().getYear(), tipoLicencias.get(i).getCantidadMaximaAnual(), tipoLicencias.get(i));
+            remanenteDiasLicencias.add(remanenteDiasLicencia);
+        }
+        remanenteDiasLicenciasRepository.saveAll(remanenteDiasLicencias);
+        return remanenteDiasLicencias;
     }
 
 }
