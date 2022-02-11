fc71f4e365c22148faaef5ba36ac2b17060e6875
francogaliotti <67112060+francogaliotti@users.noreply.github.com>
2022-02-10 22:47:20 -0300

    Creacion de licencia y boleta, correccion de bugs IV

--- src/main/java/com/SIGER/SIGER/common/PaginatedResultsHeaderUtils.java
+++ src/main/java/com/SIGER/SIGER/common/PaginatedResultsHeaderUtils.java
@@ -8,7 +8,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Component("paginatedResultsHeaderUtils")
 public class PaginatedResultsHeaderUtils {
 
-  public static final int PAGE_SIZE = 25;
+  public static final int PAGE_SIZE = 100;
 
   private static final String PAGE_PARAM = "page";
   private static final String NEXT_REL = "next";
